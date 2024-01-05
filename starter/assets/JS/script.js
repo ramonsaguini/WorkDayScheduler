2f3b211d35569f656bd0155ea3aa54ee13553a35
Ramon Andrade <124772095+ramonsaguini@users.noreply.github.com>
2024-01-05 19:05:48 +0000

    func create time block = insert for loop to create all time blocks + If to insert class pass, pres + fut

--- starter/assets/JS/script.js
+++ starter/assets/JS/script.js
@@ -6,3 +6,25 @@ $(document).ready(function () {
 
   dateAndTime();
 });
+
+function createTimeBlock() {
+  var container = $("#timeblock"); //!will be create in for Loop
+
+  var currentHour = dayjs().hour();
+
+  for (var hour = 8; hour <= 17; hour++) {
+    var timeBlock = $("<div>").addClass("row time-block"); //!create block Hour
+    var hourCol = $("<div>").addClass("col-md-1 hour").text(dayjs().hour(hour).format("hA")); //! collunm to show the hour
+    var eventCol = $("<div>").addClass("col-md-10 description"); //!col description
+    var eventTextarea = $("<textarea>").attr("data-hour", hour);//! insert data / hour
+
+    if (hour < currentHour) {
+      eventTextarea.addClass('past');
+    } else if (hour === currentHour) {
+      eventTextarea.addClass(present');
+    } else {
+
+    }
+
+  }
+}
